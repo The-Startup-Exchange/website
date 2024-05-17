63d5082b09eece6dc494f6a359d780e5a63f5721
cartercote <cartercote06@gmail.com>
2024-05-17 18:27:13 -0400

    added program logos and links

--- components/AlumniPrograms.tsx
+++ components/AlumniPrograms.tsx
@@ -1,43 +1,59 @@
 import Link from "next/link";
+import Image from "next/image";
 import {
     plus_jakarta_sans_medium,
 } from '../app/fonts';
 
 const programLogos = [
-    { text: "Link 1", href: "/link1" },
-    { text: "Link 2", href: "/link2" },
-    { text: "Link 3", href: "/link3" },
-    { text: "Link 4", href: "/link4" }
+    { name: "forbes", href: "https://forbes.com/" },
+    { name: "buildspace", href: "https://buildspace.so/" },
+    { name: "kp", href: "https://fellows.kleinerperkins.com/" },
+    { name: "contrary", href: "https://www.contrary.com/partners" },
+
+    { name: "zfellows", href: "https://www.zfellows.com/" },
+    { name: "8vc", href: "https://www.8vc.com/fellowships" },
+    { name: "antler", href: "https://www.antler.co/" },
+    { name: "commit", href: "" },
+
+    { name: "neo", href: "https://neo.com/" },
+    { name: "ondeck", href: "https://www.beondeck.com/" },
+    { name: "reactor", href: "https://www.floodgate.com/" },
+    { name: "unusual", href: "https://www.unusual.vc/" },
 ];
 
 const AlumniPrograms = () => {
-  return (
-    <div className="flex flex-col w-[94%] py-[75px] border-[#242424] border-x items-start justify-start text-6xl font-bold gap-y-4 ">
-    <div className="flex flex-col w-full">
-      <div
-        className="flex flex-col w-full px-12 items-start justify-start lg:space-y-10 space-y-0">
-          <h1 className={`text-[#FFFFFF] w-[85%] text-[58px] leading-[125%] ${plus_jakarta_sans_medium.className}`}>SX alumni are backed by the best.</h1>
-          <h2 className={`text-[#FFFFFF] w-[85%] text-[24px] leading-[125%] ${plus_jakarta_sans_medium.className}`}>→ See what members are up to this summer.</h2>
-      </div>
-      <div className="flex flex-col my-[75px] border-[#323232] border-dashed border-t items-start justify-start">
-        {Array.from({ length: 4 }, () => (
-          <div className="flex flex-row w-full border-b border-dashed border-[#323232] items-start">
-            {programLogos.map(item => (
-              <div className="flex flex-row w-full border-r border-dashed border-[#323232] py-24 px-12 align-center items-center justify-center">
-                <Link href={item.href}>
-                  <h1 className={`text-[#FFFFFF] text-[58px] leading-[125%] ${plus_jakarta_sans_medium.className}`}>
-                    {item.text}
-                  </h1>
-                </Link>
+    const numRows = Math.ceil(programLogos.length / 4);
+  
+    return (
+      <div className="flex flex-col w-[94%] py-[75px] border-[#242424] border-x items-start justify-start text-6xl font-bold gap-y-4">
+        <div className="flex flex-col w-full">
+          <div className="flex flex-col w-full px-12 items-start justify-start lg:space-y-10 space-y-0">
+            <h1 className={`text-[#FFFFFF] w-[85%] text-[58px] leading-[125%] ${plus_jakarta_sans_medium.className}`}>SX alumni are backed by the best.</h1>
+            <h2 className={`text-[#FFFFFF] w-[85%] text-[24px] leading-[125%] ${plus_jakarta_sans_medium.className}`}>→ See what members are up to this summer.</h2>
+          </div>
+          <div className="flex flex-col my-[75px] border-[#323232] border-dashed border-t items-start justify-start">
+            {Array.from({ length: numRows }, (_, rowIndex) => (
+              <div className="flex flex-row w-full border-b border-dashed border-[#323232]">
+                {programLogos.slice(rowIndex * 4, (rowIndex + 1) * 4).map(item => (
+                  <div className="flex flex-row w-full border-r border-dashed border-[#323232] py-24 px-12 align-center items-center justify-center">
+                    <Link href={item.href}>
+                      <Image
+                        src={`/${item.name}.png`}
+                        alt={item.name}
+                        width={200}
+                        height={200}
+                        className="opacity-80 hover:opacity-100 transition duration-500"
+                      />
+                    </Link>
+                  </div>
+                ))}
               </div>
             ))}
           </div>
-        ))}
+        </div>
       </div>
-    </div>
-  </div>
-  )
-}
-
-export default AlumniPrograms
+    );
+  }
+  
+  export default AlumniPrograms;
 
--- components/BottomMenu.tsx
+++ components/BottomMenu.tsx
@@ -9,11 +9,11 @@ const BottomMenu = () => {
         <div className="flex flex-col w-[94%] py-[52px] border-[#242424] border-x items-center justify-center">
           <div className="flex flex-col px-28 py-28 items-center align-center justify-center w-full">
             <Image
-                    src="/sxLogo.png"
-                    alt="SX Full Logo"
-                    width={200}
-                    height={200}
-                    />
+                src="/sxLogo.png"
+                alt="SX Full Logo"
+                width={200}
+                height={200}
+            />
             <h1 className={`text-[#FFFFFF] text-[14px] leading-[125%] ${plus_jakarta_sans_extrabold.className}`}>STARTUP EXCHANGE</h1>
           </div>
           <div className="flex flex-row w-full items-center align-center justify-center">
