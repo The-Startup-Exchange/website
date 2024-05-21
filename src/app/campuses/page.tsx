1210e607b6ab6efaed3595a4dde1f3b23e139205
cartercote <cartercote06@gmail.com>
2024-05-21 18:30:59 -0400

    fixed light mode gradients

--- src/app/campuses/page.tsx
+++ src/app/campuses/page.tsx
@@ -48,7 +48,7 @@ const Campuses = () => {
             <Header />
             <div className={`flex flex-col w-full items-center justify-center ${theme === 'dark' ? 'text-white bg-black' : 'text-black bg-white'}`}>
                 <div className={`flex flex-col w-[94%] py-32 px-12 ${theme === 'dark' ? 'border-[#242424] bg-dot-white/[0.2]' : 'border-[#E0E0E0] bg-dot-black/[0.2]'} border-x items-start justify-center gap-y-4`}>
-                    <div className={`absolute pointer-events-none inset-0 flex items-center justify-center ${theme === 'dark' ? 'bg-black' : 'bg-white'} [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]`}></div>
+                    <div className={`absolute pointer-events-none inset-0 flex items-center justify-center ${theme === 'dark' ? 'bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_30%,black)]' : 'bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_60%,white)]'}`}></div>
                     <h1 className={`w-full text-start text-[21px] pb-16 leading-[125%] ${messina_book.className}`}>CAMPUSES</h1>
                     <div className={`flex flex-col md:flex-row w-full md:space-x-16 space-y-8 md:space-y-0 md:items-center text-start ${plus_jakarta_sans_regular.className}`}>
                         <p className="md:text-[48px] text-[32px] leading-[125%] ">
