d02c85a31b9a2dd36237079ce465fa50080343f2
cartercote <cartercote06@gmail.com>
2024-05-29 00:40:34 -0400

    added posthog

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -10,6 +10,9 @@ import { Badge, badgeVariants } from "@/components/ui/badge";
 import Image from "next/image";
 import Images from '../../assets/images';
 import { Button } from "@/components/ui/button";
+import posthog from 'posthog-js'
+
+posthog.init(process.env.POSTHOG_API_KEY!, { api_host: 'https://us.i.posthog.com' })
 
 const Hero = () => {
     const { theme, toggleTheme } = useTheme();
@@ -33,7 +36,14 @@ const Hero = () => {
                         <h1 className={`md:w-[85%] w-[100%] md:text-[70px] text-[48px] leading-[110%] tracking-tighter ${plus_jakarta_sans_extrabold.className} ${theme === 'dark' ? 'bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-500' : 'text-black'}`}>The path to moving your ideas forward awaits.</h1>
                         <p className={`md:text-[24px] text-[18px] leading-[150%] ${plus_jakarta_sans_regular.className} ${theme === 'dark' ? 'text-gray-300' : 'text-gray-900'}`}>Startup Exchange is a student-led organization curating the largest community for college builders, makers, and creators. We enable you to build, launch, and grow your ideas. 
                         Sign up to hear about cool builders, campus communities, and upcoming events.</p>
-                        <Button variant="secondary" className="px-8" href="/about">Our story</Button>
+                        <Button 
+                            variant="secondary" 
+                            className="px-8" 
+                            href="/about"
+                            onClick={() => posthog.capture('our_story_button_clicked', {property: 'value'})}
+                        >
+                            Read our story
+                        </Button>
                     </div>
                 </div>
             </div>
