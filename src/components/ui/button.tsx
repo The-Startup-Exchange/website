2601483cb30fe413284f4f1cfb1de061404d86dd
cartercote <cartercote06@gmail.com>
2024-05-28 20:27:28 -0400

    our story button for now

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,10 +3,11 @@ import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 import { useTheme } from '../../context/ThemeContext';
+import { messina_semibold } from '../../app/fonts';
 import Link from "next/link";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-xl text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  `inline-flex items-center justify-center whitespace-nowrap rounded-xl text-sm ${messina_semibold.className} ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50`,
   {
     variants: {
       variant: {
