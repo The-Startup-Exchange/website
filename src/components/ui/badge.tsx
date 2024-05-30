da444136f5931ec617467e36e36ea7ea27f6523f
cartercote <cartercote06@gmail.com>
2024-05-30 15:49:42 -0400

    slight badge fix

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const badgeVariants = cva(
-  "inline-flex items-center rounded-full border px-4 py-1 text-xs tracking-tight stext-white font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+  "inline-flex items-center rounded-full border px-4 py-1 md:text-xs text-[11.75px] tracking-tight stext-white font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
   {
     variants: {
       variant: {
